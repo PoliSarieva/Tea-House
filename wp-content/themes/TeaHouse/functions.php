06a30ec294a9440275204d0ef32f1eafcf607fe7
PoliSarieva <109367470+PoliSarieva@users.noreply.github.com>
2024-02-11 23:06:30 +0200

    create dinamacally footer sidebar

--- wp-content/themes/TeaHouse/functions.php
+++ wp-content/themes/TeaHouse/functions.php
@@ -43,7 +43,11 @@ add_action('wp_enqueue_scripts', 'my_load_scripts');
  function tea_register_nav_menus() {
     register_nav_menus(
         array(
-        'header-menu' => __( 'Header Menu' ),
+        'primary-menu'    => __( 'Primary Menu' ),
+        'Our-Office'     => __( 'Our Office' ),
+        'quick-links'    => __( 'Quick Links' ),
+        'business-hours' => __( 'Business Hours' ),
+        'newsletter'     => __( 'Newsletter' ),
         )
     );
 }
@@ -56,30 +60,54 @@ add_action('wp_enqueue_scripts', 'my_load_scripts');
      * @return void
      */
     
-// function softuni_sidebars() {
-//     register_sidebar(
-//         array(
-//             'id'            => 'footer-1',
-//             'name'          => __( 'Footer 01' ),
-//             'description'   => __( 'A short description of the sidebar.' ),
-//             'before_widget' => '<div id="%1$s" class="widget %2$s">',
-//             'after_widget'  => '</div>',
-//             'before_title'  => '<h3 class="widget-title">',
-//             'after_title'   => '</h3>',
-//         )
-//     );
+function softuni_sidebars() {
+    register_sidebar(
+        array(
+            'id'            => 'footer-1',
+            'name'          => __( 'Footer 01' ),
+            'description'   => __( 'A short description of the sidebar.' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
 
-//     register_sidebar(
-//         array(
-//             'id'            => 'footer-2',
-//             'name'          => __( 'Footer 02' ),
-//             'description'   => __( 'A short description of the sidebar.' ),
-//             'before_widget' => '<div id="%1$s" class="widget %2$s">',
-//             'after_widget'  => '</div>',
-//             'before_title'  => '<h3 class="widget-title">',
-//             'after_title'   => '</h3>',
-//         )
-//     );
-// }
+    register_sidebar(
+        array(
+            'id'            => 'footer-2',
+            'name'          => __( 'Footer 02' ),
+            'description'   => __( 'A short description of the sidebar.' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
 
-// add_action( 'widgets_init', 'softuni_sidebars' );
+    register_sidebar(
+        array(
+            'id'            => 'footer-3',
+            'name'          => __( 'Footer 03' ),
+            'description'   => __( 'A short description of the sidebar.' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+
+    register_sidebar(
+        array(
+            'id'            => 'footer-4',
+            'name'          => __( 'Footer 04' ),
+            'description'   => __( 'A short description of the sidebar.' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+}
+
+add_action( 'widgets_init', 'softuni_sidebars' );
