e1d157e2ebfa45760100325419b02e950df09f68
PoliSarieva <109367470+PoliSarieva@users.noreply.github.com>
2024-02-12 22:19:07 +0200

    add post template part

--- wp-content/themes/TeaHouse/author.php
+++ wp-content/themes/TeaHouse/author.php
@@ -14,21 +14,7 @@
 
             <?php while( have_posts() ) : the_post(); ?>
 
-                <div class="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
-                    <?php if ( has_post_thumbnail() ) : ?>
-                            <?php the_post_thumbnail(); ?>
-                    <?php else : ?>
-                            <img class="img-fluid" src="<?php echo TEA_FILES_URL; ?>/img/article.jpg" alt="">
-                    <?php endif; ?>
-                </div>
-                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-                    <div class="section-title">
-                        <h1 class="display-6"><?php the_title(); ?></h1>
-                    </div>
-                
-                    <p class="mb-4"><?php the_content(); ?></p>
-                    <a href="" class="btn btn-primary rounded-pill py-3 px-5">Read More</a>
-                </div>
+            <?php get_template_part( 'partials/content' , 'post' ); ?>
 
             <?php endwhile; ?>
 
